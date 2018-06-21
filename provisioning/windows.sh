e5c6ee7f776304977695c7cc368173b679767e17
Vranac Srdjan <vranac@code4hire.com>
2018-06-21 11:46:06 +0200

    Make sure pip is installed, setup ansible.

--- provisioning/windows.sh
+++ provisioning/windows.sh
@@ -6,10 +6,13 @@ then
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get install -y python3-software-properties
     sudo apt-get install -y python3-setuptools
+    sudo apt-get install -y python3-pip
 
     echo "Installing Ansible"
-    sudo easy_install pip
-    sudo pip install ansible
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+    sudo add-apt-repository http://ppa.launchpad.net/ansible/ansible/ubuntu
+    sudo apt-get update
+    sudo apt-get install -y ansible
     echo "Ansible installed"
 fi
 
