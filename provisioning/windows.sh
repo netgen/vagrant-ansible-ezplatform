399279ca784dd0db944c4d13de01971a4df4984e
Vranac Srdjan <vranac@code4hire.com>
2018-06-26 13:48:44 +0200

    Checks to save time failed in this os version, remove them.

--- provisioning/windows.sh
+++ provisioning/windows.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-    echo "Add APT repositories"
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get install -y python3-software-properties
-    sudo apt-get install -y python3-setuptools
-    sudo apt-get install -y python3-pip
+echo "Add APT repositories"
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y python3-software-properties
+sudo apt-get install -y python3-setuptools
+sudo apt-get install -y python3-pip
 
-    echo "Installing Ansible"
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-    sudo add-apt-repository http://ppa.launchpad.net/ansible/ansible/ubuntu
-    sudo apt-get update
-    sudo apt-get install -y ansible
-    echo "Ansible installed"
-fi
+echo "Installing Ansible"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo add-apt-repository http://ppa.launchpad.net/ansible/ansible/ubuntu
+sudo apt-get update
+sudo apt-get install -y ansible
+echo "Ansible installed"
 
 ansible-playbook /vagrant/provisioning/vagrant.yml -c local -i localhost,
